baac69dd9a21d1cf3bb162f78260b201407c930e
Dzimitry Lvou <lvoudzimitry@gmail.com>
2021-02-25 20:43:00 +0300

    feat: 100 score

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr) {
+  if (!Array.isArray(arr) || arr.length == 0) return 0;
+  let min = arr[0]
+  for (let index = 1; index < arr.length; index++) {
+    if (arr[index] < min) {
+      min = arr[index]
+    }
+    
+  }
+  return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr) {
+  if (!Array.isArray(arr) || arr.length == 0) return 0;
+  let max = arr[0]
+  for (let index = 1; index < arr.length; index++) {
+    if (arr[index] > max) {
+      max = arr[index]
+    }
+    
+  }
+  return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr) {
+  if (!Array.isArray(arr) || arr.length == 0) return 0;
+  let sum = 0
+  for (let index = 0; index < arr.length; index++) {
+    sum += arr[index]
+  }
+  return sum / arr.length;
 }
